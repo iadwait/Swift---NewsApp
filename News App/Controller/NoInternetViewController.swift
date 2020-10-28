fd66a8f0bd6ff278c2b9498f4b7ac511ca630b03
iAdwait <adwaitbarkale@gmail.com>
2020-10-28 16:55:15 +0530

    Branch Created

---
+++ News App/Controller/NoInternetViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NoInternetViewController.swift
+//  News App
+//
+//  Created by Adwait Barkale on 28/10/20.
+//  Copyright © 2020 Adwait Barkale. All rights reserved.
+//
+
+import UIKit
+
+class NoInternetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
